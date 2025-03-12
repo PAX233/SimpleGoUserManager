fe09cc11861128e28c290dd0f71f247b10a72324
PAX233 <jaysondun@hotmail.com>
2025-03-12 20:50:15 +0800

    v0.1

--- main.go
+++ main.go
@@ -66,29 +66,132 @@ func addCustomer() {
 
 		fmt.Printf("用户信息：%v\n", newCustomer)
 
-		fmt.Println("是否继续添加？(y/n)")
-		var choice string
-		fmt.Scan(&choice)
-		if choice == "n" {
-			fmt.Printf("\033[1;30;42m%s\033[0m\n",`--------添加结束--------`)
-			break
+		for {
+			fmt.Println("是否继续添加？(y/n)")
+			var choice string
+			fmt.Scan(&choice)
+			if choice == "n" {
+				fmt.Printf("\033[1;30;42m%s\033[0m\n", `--------添加结束--------`)
+				return
+			} else if choice == "y" {
+				break
+			} else {
+				fmt.Println("输入有误")
+			}
+
 		}
 	}
 }
 
 // 查看客户
 func showCustomer() {
-	fmt.Println("查看客户:")
+	fmt.Printf("\033[1;30;42m%s\033[0m\n", `--------查看客户--------`)
+
+	// 打印表头
+	fmt.Printf("%-4s | %-10s | %-4s | %-6s | %-20s\n", "ID", "姓名", "年龄", "性别", "邮箱")
+	fmt.Println("----------------------------------------------------")
+	if len(customers) == 0 {
+		fmt.Println("暂无客户信息")
+		return
+	} else
+	// 打印客户信息
+	{
+		for _, customer := range customers {
+			fmt.Printf("%-4s | %-10s | %-4s | %-6s | %-20s\n",
+				customer["cid"],
+				customer["name"],
+				customer["age"],
+				customer["gender"],
+				customer["email"])
+		}
+	}
+	fmt.Printf("\033[1;30;42m%s\033[0m\n", `--------查看结束--------`)
 }
 
 // 更新客户
 func updateCustomer() {
-	fmt.Println("更新客户:")
+	fmt.Printf("\033[1;30;42m%s\033[0m\n", `--------更新客户--------`)
+
+	fmt.Println("请输入要更新的客户id：")
+	var cid string
+	fmt.Scan(&cid)
+	index := -1
+	for i, v := range customers {
+		if v["cid"] == cid {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		fmt.Println("没有找到该客户")
+		return
+	}
+
+	// 更新客户信息
+	var name string
+	fmt.Println("请输入新的客户姓名（当前：" + customers[index]["name"] + "）：")
+	fmt.Scan(&name)
+
+	var age int
+	fmt.Println("请输入新的客户年龄（当前：" + customers[index]["age"] + "）：")
+	fmt.Scan(&age)
+
+	var gender string
+	fmt.Println("请输入新的客户性别（当前：" + customers[index]["gender"] + "）：")
+	fmt.Scan(&gender)
+
+	var email string
+	fmt.Println("请输入新的客户邮箱（当前：" + customers[index]["email"] + "）：")
+	fmt.Scan(&email)
+
+	// 更新客户信息
+	customers[index]["name"] = name
+	customers[index]["age"] = strconv.Itoa(age)
+	customers[index]["gender"] = gender
+	customers[index]["email"] = email
+
+	fmt.Println("更新成功")
+	fmt.Printf("更新后的用户信息：%v\n", customers[index])
+	fmt.Printf("\033[1;30;42m%s\033[0m\n", `--------更新结束--------`)
 }
 
 // 删除客户
 func deleteCustomer() {
-	fmt.Println("删除客户:")
+	for {
+		fmt.Printf("\033[1;30;42m%s\033[0m\n", `--------删除客户--------`)
+
+		fmt.Println("请输入要删除的客户id：")
+		var cid string
+		fmt.Scan(&cid)
+		index := -1
+		for i, v := range customers {
+			if v["cid"] == cid {
+				index = i
+				break
+			}
+		}
+
+		if index == -1 {
+			fmt.Println("没有找到该客户")
+		} else {
+			customers = append(customers[:index], customers[index+1:]...)
+			fmt.Println("删除成功")
+		}
+		for {
+			fmt.Println("是否继续删除？(y/n)")
+			var choice string
+			fmt.Scan(&choice)
+			if choice == "n" {
+				fmt.Printf("\033[1;30;42m%s\033[0m\n", `--------删除结束--------`)
+				break
+			} else if choice == "y" {
+				break
+			} else {
+				fmt.Println("输入有误")
+			}
+		}
+	}
 }
 
 // 保存客户
@@ -104,20 +207,25 @@ func main() {
 		fmt.Print("请输入选择(1-6)：")
 		fmt.Scanln(&userChoice)
 
-		fmt.Println(userChoice)
+		// fmt.Println(userChoice)
 
 		// 调用对应的函数
 		switch userChoice {
 		case 1:
 			addCustomer()
+			break
 		case 2:
 			showCustomer()
+			break
 		case 3:
 			updateCustomer()
+			break
 		case 4:
 			deleteCustomer()
+			break
 		case 5:
 			saveCustomer()
+			break
 		case 6:
 			os.Exit(0)
 		default:
