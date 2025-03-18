8a33eeff74ca786472b791192758da9fa0b39314
PAX233 <jaysondun@hotmail.com>
2025-03-18 20:36:22 +0800

    refactor(menu): 优化菜单显示逻辑并移除冗余代码
    移除不必要的 `break` 语句，优化菜单显示格式，并添加延时返回菜单功能以提升用户体验

--- menu/menu.go
+++ menu/menu.go
@@ -1,24 +1,26 @@
 package menu
 
 import (
+	"basicSystem/customer"
 	"fmt"
 	"os"
-
-	"basicSystem/customer"
+	"time"
 )
 
 // ShowMenu 显示主菜单
 func ShowMenu() {
 	fmt.Printf("\033[1;30;42m%s\033[0m\n", `
---------客户信息管理系统--------
-	1、添加客户
-	2、查看客户
-	3、更新客户
-	4、删除客户
-	5、保存
-	6、退出
-----------------------------
-	`)
+════════════════════════════
+    客户信息管理系统       
+════════════════════════════
+    1、添加客户           
+    2、查看客户           
+    3、更新客户           
+    4、删除客户           
+    5、保存               
+    6、退出               
+════════════════════════════
+`)
 }
 
 // HandleUserChoice 处理用户选择
@@ -34,23 +36,19 @@ func HandleUserChoice() {
 		switch userChoice {
 		case 1:
 			customer.AddCustomer()
-			break
 		case 2:
 			customer.ShowCustomer()
-			break
 		case 3:
 			customer.UpdateCustomer()
-			break
 		case 4:
 			customer.DeleteCustomer()
-			break
 		case 5:
 			customer.SaveCustomer()
-			break
 		case 6:
 			os.Exit(0)
 		default:
 			fmt.Println("输入有误")
 		}
+		time.Sleep(2 * time.Second) // 延时返回菜单
 	}
 }
